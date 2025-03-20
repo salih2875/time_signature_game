b8d2499948870a7bd4fb094ce77618ca0ed9305a
salih2875 <139330615+salih2875@users.noreply.github.com>
2025-03-20 05:56:34 +0300

    Fixed padding

--- src/App.tsx
+++ src/App.tsx
@@ -383,7 +383,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10px;
+  // padding: 10px;
   box-sizing: border-box;
   overflow: hidden; /* Prevents scrollbars */
 `;
